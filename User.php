c63253290ca77c9b713479559d1ab6f1d3e4a346
sararrm2 <120378920+sararrm2@users.noreply.github.com>
2023-08-31 15:22:23 +0100

    Add files via upload

---
+++ User.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\PedagogicalGroup;
+use App\Ufcd;
+use App\UserType;
+use App\TeacherAvailability;
+use App\SpecializationArea;
+use App\ScheduleAtribution;
+
+class User extends Authenticatable implements MustVerifyEmail
+
+{
+    use SoftDeletes;
+    
+    use Notifiable;
+
+    public function pedagogicalGroups()
+    {
+        return $this->belongsToMany(PedagogicalGroup::class, 'pedagogical_group_users');
+    }
+    
+    public function ufcds()
+    {
+        return $this->belongsToMany(Ufcd::class, 'user_ufcds');
+    }
+
+    public function userType()
+    {
+        return $this->belongsTo(UserType::class);
+    }
+
+    public function teacherAvailabilities()
+    {
+        return $this->hasMany(TeacherAvailability::class);
+    }
+
+    public function specializationAreas()
+    {
+        return $this->belongsToMany(SpecializationArea::class, 'specialization_area_users', 'user_id', 'specialization_area_number');
+    }
+
+    public function scheduleAtributions()
+    {
+        return $this->hasMany(ScheduleAtribution::class);
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}

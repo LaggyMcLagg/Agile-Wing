8f5715b781d966314d5a655115065b0d187ffd45
Velgrieve <Ines.Paiva.T0124276@edu.atec.pt>
2023-09-28 17:16:22 +0100

    Commit WIP
    Sprint III BL

--- Projecto/AgileWing/public/js/course_class_table.js
+++ Projecto/AgileWing/public/js/course_class_table.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -93,190 +93,18 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
-/**
-* Handles the behavior of the course control form.
-* 
-* This script manages:
-* - The toggling between editing and view modes.
-* - The persistence of form states across page reloads using session storage.
-* - Transferring data from table rows to the form for potential editing.
-* - Storing certain form states and selections in session storage.
-*
-* @author   [Vasco Vitória]
-*/
-// Wait until the DOM is fully loaded
-document.addEventListener("DOMContentLoaded", function () {
-  // Getting elements from the DOM
-  var controlForm = document.getElementById("controlForm");
-  // Store the initial action URL
-  var baseAction = controlForm.action;
-  var editBtn = document.getElementById("editBtn");
-  var createBtn = document.getElementById("createBtn");
-  var saveBtn = document.getElementById("saveBtn");
-  var cancelBtn = document.getElementById("cancelBtn");
-  var idLabel = document.getElementById("id_label");
-  var tableRows = document.querySelectorAll("tbody tr");
-
-  // Flag to check if the form is in editing mode
-  var isEditing = false;
-
-  // Load the form state from session storage
-  var formState = sessionStorage.getItem("formState");
-  if (formState === "edit") {
-    enableEdit(); // Enable the edit mode
-
-    var storedCourseId = sessionStorage.getItem("selectedCourseId");
-    if (storedCourseId) {
-      idLabel.textContent = storedCourseId;
-    }
-
-    // Set the method and action for the form when a row is selected
-    document.getElementById("hiddenMethod").value = "PUT";
-    controlForm.action = baseAction + '/' + storedCourseId;
-  } else if (formState === "create") {
-    enableEdit(); // Enable the create mode
-  } else {
-    disableEdit(); // By default, keep the form disabled
-  }
-
-  // Function to reset the form
-  function clearForm() {
-    // Clear all inputs in the form
-    document.querySelectorAll('#controlForm [data-name]').forEach(function (input) {
-      input.value = '';
-    });
-    sessionStorage.removeItem("selectedCourseId"); // Clear the stored course ID
-  }
-
-  // Function to enable the form editing mode
-  function enableEdit() {
-    document.querySelectorAll('#controlForm input, #controlForm select').forEach(function (input) {
-      input.removeAttribute('readonly');
-      input.removeAttribute('disabled');
-    });
-    document.querySelectorAll('#ufcdsCheckboxList input[type="checkbox"]').forEach(function (checkbox) {
-      checkbox.removeAttribute('disabled');
-    });
-    saveBtn.style.display = 'block';
-    cancelBtn.style.display = 'block';
-    isEditing = true;
-  }
-
-  // Function to disable the form editing mode
-  function disableEdit() {
-    document.querySelectorAll('#controlForm input, #controlForm select').forEach(function (input) {
-      input.setAttribute('readonly', true);
-      input.setAttribute('disabled', true);
-    });
-    document.querySelectorAll('#ufcdsCheckboxList input[type="checkbox"]').forEach(function (checkbox) {
-      checkbox.setAttribute('disabled', true);
-    });
-    saveBtn.style.display = 'none';
-    cancelBtn.style.display = 'none';
-    isEditing = false;
-  }
-
-  // On "Edit" button click, enable editing if not already editing
-  editBtn.addEventListener("click", function () {
-    sessionStorage.setItem("formState", "edit"); // Store the EDIT state to session storage
-    if (!isEditing) {
-      enableEdit();
-    }
-  });
-
-  // On "Create" button click, reset the form and enable editing
-  createBtn.addEventListener("click", function () {
-    clearForm();
-    enableEdit();
-    document.getElementById("hiddenMethod").value = "POST";
-    controlForm.action = baseAction;
-    sessionStorage.setItem("formState", "create"); // Store the CREATE state to session storage
-  });
-
-  // On "Cancel" button click, reset the form and disable editing
-  cancelBtn.addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent any default form submission
-    clearForm();
-    disableEdit();
-    sessionStorage.removeItem("formState"); // Clear the state from session storage
-  });
-
-  tableRows.forEach(function (row) {
-    row.addEventListener("click", function () {
-      if (!isEditing) {
-        // Get the ID from the clicked row
-        var id = row.querySelector('[data-name="id"]').textContent;
-        idLabel.textContent = id;
-        sessionStorage.setItem("selectedCourseId", id);
-        row.querySelectorAll('[data-name]').forEach(function (cell) {
-          var fieldName = cell.getAttribute('data-name');
-          var formInput = document.querySelector("#controlForm [data-name=\"".concat(fieldName, "\"]"));
-          if (formInput) {
-            switch (formInput.getAttribute('data-type')) {
-              case 'comboBox':
-                if (formInput.tagName === 'SELECT') {
-                  var value = cell.textContent.trim();
-                  var _iterator = _createForOfIteratorHelper(formInput.options),
-                    _step;
-                  try {
-                    for (_iterator.s(); !(_step = _iterator.n()).done;) {
-                      var option = _step.value;
-                      if (option.text === value) {
-                        formInput.value = option.value;
-                        break;
-                      }
-                    }
-                  } catch (err) {
-                    _iterator.e(err);
-                  } finally {
-                    _iterator.f();
-                  }
-                }
-                break;
-              case 'checkBoxList':
-                var ufcdListDiv = document.getElementById("ufcdsList_".concat(id));
-
-                // Fetch the values of the <li> elements instead of the text
-                var ufcdIds = Array.from(ufcdListDiv.querySelectorAll('li')).map(function (li) {
-                  return li.getAttribute('value');
-                });
-                var ufcdCheckboxes = document.querySelectorAll('#ufcdsCheckboxList input[type="checkbox"]');
-                ufcdCheckboxes.forEach(function (checkbox) {
-                  if (ufcdIds.includes(checkbox.value)) {
-                    checkbox.checked = true;
-                  } else {
-                    checkbox.checked = false;
-                  }
-                });
-                break;
-              default:
-                formInput.value = cell.textContent;
-                break;
-            }
-          }
-        });
-
-        // Set the method and action for the form when a row is selected
-        document.getElementById("hiddenMethod").value = "PUT";
-        controlForm.action = baseAction + '/' + id;
-      }
-    });
-  });
-});
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\resources\\js\\logic\\course_class_table.js: Unexpected token (1:0)\n\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 1 |\u001b[39m \u001b[33m<<\u001b[39m\u001b[33m<<\u001b[39m\u001b[33m<<\u001b[39m\u001b[33m<\u001b[39m \u001b[33mHEAD\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m   |\u001b[39m \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 2 |\u001b[39m \u001b[90m/**\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 3 |\u001b[39m \u001b[90m* Handles the behavior of the course control form.\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 4 |\u001b[39m \u001b[90m* \u001b[39m\u001b[0m\n    at instantiate (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:63:32)\n    at constructor (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:358:12)\n    at Parser.raise (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:3207:19)\n    at Parser.unexpected (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:3237:16)\n    at Parser.parseExprAtom (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:11238:16)\n    at Parser.parseExprSubscripts (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:10844:23)\n    at Parser.parseUpdate (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:10827:21)\n    at Parser.parseMaybeUnary (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:10803:23)\n    at Parser.parseMaybeUnaryOrPrivate (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:10641:61)\n    at Parser.parseExprOps (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:10646:23)\n    at Parser.parseMaybeConditional (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:10623:23)\n    at Parser.parseMaybeAssign (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:10584:21)\n    at Parser.parseExpressionBase (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:10538:23)\n    at C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:10534:39\n    at Parser.allowInAnd (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:12229:16)\n    at Parser.parseExpression (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:10534:17)\n    at Parser.parseStatementContent (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:12687:23)\n    at Parser.parseStatementLike (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:12538:17)\n    at Parser.parseModuleItem (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:12515:17)\n    at Parser.parseBlockOrModuleBlockBody (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:13139:36)\n    at Parser.parseBlockBody (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:13132:10)\n    at Parser.parseProgram (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:12414:10)\n    at Parser.parseTopLevel (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:12404:25)\n    at Parser.parse (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:14300:10)\n    at parse (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\parser\\lib\\index.js:14341:38)\n    at parser (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\core\\lib\\parser\\index.js:41:34)\n    at parser.next (<anonymous>)\n    at normalizeFile (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\core\\lib\\transformation\\normalize-file.js:64:38)\n    at normalizeFile.next (<anonymous>)\n    at run (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\core\\lib\\transformation\\index.js:21:50)\n    at run.next (<anonymous>)\n    at transform (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\core\\lib\\transform.js:22:41)\n    at transform.next (<anonymous>)\n    at step (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\gensync\\index.js:261:32)\n    at C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\gensync\\index.js:273:13\n    at async.call.result.err.err (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\gensync\\index.js:223:11)\n    at C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\gensync\\index.js:189:28\n    at C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\@babel\\core\\lib\\gensync-utils\\async.js:68:7\n    at C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\gensync\\index.js:113:33\n    at step (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\gensync\\index.js:287:14)\n    at C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\gensync\\index.js:273:13\n    at async.call.result.err.err (C:\\LOCAL REPOS\\Agile-Wing\\Projecto\\AgileWing\\node_modules\\gensync\\index.js:223:11)");
 
 /***/ }),
 
-/***/ 3:
+/***/ 4:
 /*!********************************************************!*\
   !*** multi ./resources/js/logic/course_class_table.js ***!
   \********************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\ATEC\repos\Agile-Wing\Projecto\AgileWing\resources\js\logic\course_class_table.js */"./resources/js/logic/course_class_table.js");
+module.exports = __webpack_require__(/*! C:\LOCAL REPOS\Agile-Wing\Projecto\AgileWing\resources\js\logic\course_class_table.js */"./resources/js/logic/course_class_table.js");
 
 
 /***/ })

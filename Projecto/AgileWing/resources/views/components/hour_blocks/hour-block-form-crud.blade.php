2a1e4a3f8ffb7fa31582ddfe36d8b5467b039e10
Velgrieve <Ines.Paiva.T0124276@edu.atec.pt>
2023-10-17 17:12:15 +0100

    Inicial CSS Commit
    CSS Para as págians:
    user-types
    hour-blocks
    course-classes
    availability-type

--- Projecto/AgileWing/resources/views/components/hour_blocks/hour-block-form-crud.blade.php
+++ Projecto/AgileWing/resources/views/components/hour_blocks/hour-block-form-crud.blade.php
@@ -3,40 +3,39 @@
 @endsection
 
 <!-- Start of Hour Blocks List Section -->
-<h3>Lista de Blocos de Horário - LIST</h3>
 
 <!-- If there's a success session message, display it within a styled alert box -->
 @if (session('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ session('success') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    <script>
-        sessionStorage.removeItem("formState");  // Clear the state from local storage
-        sessionStorage.removeItem("selectedId");  // Clear the stored course ID
-    </script>
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    {{ session('success') }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+<script>
+    sessionStorage.removeItem("formState"); // Clear the state from local storage
+    sessionStorage.removeItem("selectedId"); // Clear the stored course ID
+</script>
 @endif
 
 @if (session('error'))
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        {{ session('error') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    {{ session('error') }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
 @endif
 
 <!-- Start of the Main Container -->
-<div class="container" id="listForm">
-    <div class="row"> 
+<div class="container spacing" id="listForm">
+    <div class="row">
         <!-- Left Column: Form section for block timings -->
-        <div class="col-md-6"> 
+        <div class="col-md-4">
             <!-- Unified Form for CRUD operations -->
-            <form  action="{{ route('hour-blocks.store') }}" id="controlForm" method="POST">
+            <form class="atec-form" action="{{ route('hour-blocks.store') }}" id="controlForm" method="POST">
                 @csrf
-                
+
                 <!-- Hidden input for HTTP method override. Needed because HTML forms only support GET/POST natively and we're not using 
                 @method('PUT') to be able to switch between methods-->
                 <input type="hidden" name="_method" value="POST" id="hiddenMethod">
@@ -44,74 +43,57 @@
                 <!-- Display Block ID label -->
                 <label for="id">ID: </label>
                 <!-- The prop data-name tells js where to target to place the info collected from the table -->
-                <label 
-                data-name="id"
-                id="id_label">
+                <label data-name="id" id="id_label">
                 </label>
-            
-            <!-- Input for 'hour_beginning' -->
+
+                <!-- Input for 'hour_beginning' -->
                 <div class="form-group">
                     <label for="hour_beginning">Hora de início</label>
-                    <input
-                        data-name="hour_beginning"
-                        type="text"
-                        id="hour_beginning"
-                        name="hour_beginning"
-                        class="form-control @error('hour_beginning') is-invalid @enderror"
-                        required
-                        value="{{ old('hour_beginning') }}"
-                        readonly
-                    >
+                    <input data-name="hour_beginning" type="text" id="hour_beginning" name="hour_beginning" class="form-control @error('hour_beginning') is-invalid @enderror" required value="{{ old('hour_beginning') }}" readonly>
                     <!-- Error message for 'hour_beginning' -->
                     @error('hour_beginning')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
                     @enderror
                 </div>
 
                 <!-- Input for 'hour_end' -->
                 <div class="form-group">
                     <label for="hour_end">Hora de fim</label>
-                    <input
-                        data-name="hour_end"
-                        type="text"
-                        id="hour_end"
-                        name="hour_end"
-                        class="form-control @error('hour_end') is-invalid @enderror"
-                        required
-                        value="{{ old('hour_end') }}"
-                        readonly
-                    >
+                    <input data-name="hour_end" type="text" id="hour_end" name="hour_end" class="form-control @error('hour_end') is-invalid @enderror" required value="{{ old('hour_end') }}" readonly>
                     <!-- Error message for 'hour_end' -->
                     @error('hour_end')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
                     @enderror
                 </div>
 
                 <!-- Save and Cancel buttons, initially hidden -->
-                <button id="saveBtn" type="submit" class="mt-2 mb-5 btn btn-primary" style="display: none;">Guardar</button>
-                <button id="cancelBtn" class="mt-2 mb-5 btn btn-secondary" style="display: none;">Cancelar</button>
+                <div class="button-container">
+                    <button id="saveBtn" type="submit" class="btn save-btn" style="display: none;">Guardar</button>
+                    <button id="cancelBtn" class="btn cancel-btn" style="display: none;">Cancelar</button>
+                </div>
             </form>
         </div>
 
         <!-- Right Column: List of hour blocks -->
-        <div class="col-md-6">
+        <div class="col-md-8">
+
+            <h3 class="title">Lista de Blocos de Horário - LIST
+                <a id="createBtn" class="btn btn-blue">Criar</a>
+                <a id="editBtn" type="button" class="btn btn-blue">Editar</a>
+            </h3>
+
             <h5>Lista de Blocos</h5>
-            <table class="table table-bordered">
+            <table class="table table-borderless">
                 <!-- Table Headings -->
                 <thead>
                     <tr>
                         <th scope="col">ID</th>
                         <th scope="col">Hora início</th>
                         <th scope="col">Hora de fim</th>
-                        <th scope="col">
-                            <!-- Create and Edit Buttons -->
-                            <a id="createBtn" class="btn btn-primary">Criar</a>
-                            <a id="editBtn" type="button" class="btn btn-primary">Editar</a>
-                        </th>
                     </tr>
                 </thead>
                 <!-- Table Body: Looping through hour blocks and displaying them -->
@@ -127,7 +109,7 @@
                             <form action="{{ url('hour-blocks/' . $hourBlock->id) }}" method="POST" onsubmit="return confirm('Tem a certeza que quer apagar este registo?');">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Apagar bloco</button>
+                                <button type="submit" class="btn btn-trash"><i class="fa fa-trash"></i></button>
                             </form>
                         </td>
                     </tr>
--- Projecto/AgileWing/resources/views/components/user_types/user-type-form-crud.blade.php
+++ Projecto/AgileWing/resources/views/components/user_types/user-type-form-crud.blade.php
@@ -3,16 +3,16 @@
 @endsection
 
 @if (session('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ session('success') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    <script>
-        sessionStorage.removeItem("formState");  // Clear the state from local storage
-        sessionStorage.removeItem("selectedId");  // Clear the stored course ID
-        </script>
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    {{ session('success') }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+<script>
+    sessionStorage.removeItem("formState"); // Clear the state from local storage
+    sessionStorage.removeItem("selectedId"); // Clear the stored course ID
+</script>
 @endif
 
 @if (session('error'))
@@ -25,14 +25,13 @@
 @endif
 
 <!-- Start of the Main Container -->
-<div class="container" id="listForm">
-    <div class="row"> 
+<div class="container spacing" id="listForm">
+    <div class="row">
 
         <!-- LEFT COLUMN: CREATE/EDIT FORM -->
-        <div class="col-md-4"> 
-            <h3>Gestão de Tipos de utilizador</h3>
+        <div class="col-md-4">
             <!-- FORM -->
-            <form action="{{ route('user-types.store') }}" id="controlForm" method="POST">
+            <form class="atec-form" action="{{ route('user-types.store') }}" id="controlForm" method="POST">
                 @csrf
 
                 <!-- Hidden input for HTTP method override -->
@@ -45,40 +44,35 @@
                 <!-- UserType Name -->
                 <div class="form-group">
                     <label for="name">Nome de Tipo de Utilizador</label>
-                    <input 
-                        data-name="name" 
-                        type="text" 
-                        id="name" 
-                        name="name" 
-                        class="form-control @error('name') is-invalid @enderror"
-                        required
-                        value="{{ old('name') }}"
-                        readonly>
+                    <input data-name="name" type="text" id="name" name="name" class="form-control @error('name') is-invalid @enderror" required value="{{ old('name') }}" readonly>
                     @error('name')
-                        <div class="invalid-feedback">
-                            {{ $message }}
-                        </div>
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
                     @enderror
                 </div>
 
                 <!-- Save and Cancel buttons, initially hidden -->
-                <button id="saveBtn" type="submit" class="mt-2 mb-5 btn btn-primary" style="display: none;">Guardar</button>
-                <button id="cancelBtn" class="mt-2 mb-5 btn btn-secondary" style="display: none;">Cancelar</button>
+                <div class="button-container">
+                    <button id="saveBtn" type="submit" class="btn save-btn" style="display: none;">Guardar</button>
+                    <button id="cancelBtn" class="btn cancel-btn" style="display: none;">Cancelar</button>
+                </div>
             </form>
         </div>
 
         <!-- TABELA LIST/SHOW -->
         <div class="col-md-8">
-            <table class="table table-bordered">
+            <table class="table table-borderless">
+                <h3 class="title">Gestão de Tipos de utilizador
+                    <a id="createBtn" class="btn btn-blue">Criar</a>
+                    <a id="editBtn" type="button" class="btn btn-blue">Editar</a>
+                </h3>
+
                 <thead>
                     <tr>
                         <th scope="col">ID</th>
                         <th scope="col">Nome Tipo de Utilizador</th>
                         <!-- Add any other headers here that are similar to the Course table headers -->
-                        <th scope="col">
-                            <a id="createBtn" class="btn btn-primary">Criar</a>
-                            <a id="editBtn" type="button" class="btn btn-primary">Editar</a>
-                        </th>
                     </tr>
                 </thead>
                 <tbody>
@@ -92,7 +86,7 @@
                                 <form action="{{ url('user-types/' . $userType->id) }}" method="POST">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="btn btn-danger">Apagar</button>
+                                    <button type="submit" class="btn btn-trash"><i class="fa fa-trash"></i></button>
                                 </form>
                             </div>
                         </td>

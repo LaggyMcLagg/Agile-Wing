b7c2d2a95385f18016775c6b9700f182d5f40653
Velgrieve <Ines.Paiva.T0124276@edu.atec.pt>
2023-10-17 17:12:15 +0100

    Inicial CSS Commit
    CSS Para as págians:
    user-types
    hour-blocks
    course-classes
    availability-type

--- Projecto/AgileWing/resources/views/master/main.blade.php
+++ Projecto/AgileWing/resources/views/master/main.blade.php
@@ -18,6 +18,10 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-jN1Xgrh1+WjjoWt6AMtFA9wZ0Vd3JR6fJ4FjZ7ZxWJqrrNMeN2VCAWLTlMzGZifDfnW7zYw9l+EmLXTXvnbeEg==" crossorigin="anonymous" />
+
+    <link href="{{ asset('css/users.css') }}" rel="stylesheet">
     @yield('styles')
     
     <!-- Scripts -->

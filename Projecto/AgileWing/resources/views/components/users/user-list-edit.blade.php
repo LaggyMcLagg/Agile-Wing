faa4fc3d7264277791b2a04b998448d4138831ec
Velgrieve <Ines.Paiva.T0124276@edu.atec.pt>
2023-10-25 22:57:50 +0100

    WIP css Update

--- Projecto/AgileWing/resources/views/components/users/user-list-edit.blade.php
+++ Projecto/AgileWing/resources/views/components/users/user-list-edit.blade.php
@@ -13,53 +13,55 @@
 </div>
 
 @if (session('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ session('success') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    {{ session('success') }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
 @endif
-<table class="table table-borderless spacing" id="sortable-table">
-    <thead>
-        <tr>
-            <th data-column-index="0" scope="col">Nome do formador</th>
-            <th data-column-index="1" scope="col">Área de formação</th>
-            <th data-column-index="2" scope="col">Grupo pedagógico</th>
-            <th data-column-index="3" scope="col">Último Login</th>
-            <th data-column-index="4" scope="col">Última Gravação</th>
-            <th data-column-index="4" scope="col">Apagar</th>
+<div class="table-container">
+    <table class="table table-borderless spacing" id="sortable-table">
+        <thead>
+            <tr>
+                <th data-column-index="0" scope="col">Nome do formador</th>
+                <th data-column-index="1" scope="col">Área de formação</th>
+                <th data-column-index="2" scope="col">Grupo pedagógico</th>
+                <th data-column-index="3" scope="col">Último Login</th>
+                <th data-column-index="4" scope="col">Última Gravação</th>
+                <th data-column-index="4" scope="col">Apagar</th>
 
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($users as $user)
-        <tr class="clickable-row" data-user-id="{{ $user->id }}">
-            <td>{{ $user->name }}</td>
-            <td>
-                <ul>
-                    @foreach ($user->specializationAreas as $specializationArea)
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+            <tr class="clickable-row" data-user-id="{{ $user->id }}">
+                <td>{{ $user->name }}</td>
+                <td>
+                    <ul>
+                        @foreach ($user->specializationAreas as $specializationArea)
                         <li>{{ $specializationArea->name }}</li>
-                    @endforeach
-                </ul>
-            </td>
-            <td>
-                <ul>
-                    @foreach ($user->pedagogicalGroups as $pedagogicalGroup)
+                        @endforeach
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        @foreach ($user->pedagogicalGroups as $pedagogicalGroup)
                         <li>{{ $pedagogicalGroup->name }}</li>
-                    @endforeach
-                </ul>
-            </td>
-            <td>{{ $user->lastLogin }}</td>
-            <td>{{ $user->lastUpdated }}</td>
-            <td>
-                <form action="{{ route('users.destroy', ['user' => $user->id]) }}" method="POST" onsubmit="return confirm('Tem a certeza que quer apagar este registo?');">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-trash"><i class="fa fa-trash"></i></button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+                        @endforeach
+                    </ul>
+                </td>
+                <td>{{ $user->lastLogin }}</td>
+                <td>{{ $user->lastUpdated }}</td>
+                <td>
+                    <form action="{{ route('users.destroy', ['user' => $user->id]) }}" method="POST" onsubmit="return confirm('Tem a certeza que quer apagar este registo?');">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-trash"><i class="fa fa-trash"></i></button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
